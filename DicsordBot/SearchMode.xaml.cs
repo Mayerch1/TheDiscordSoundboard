9d458af2f3369452c45721df99fbce35407d72dd
Christian Mayer <cj_mayer@outlook.de>
2018-09-19 18:20:19 +0200

    ugly and dirty hacks for a working context menu

--- DicsordBot/SearchMode.xaml.cs
+++ DicsordBot/SearchMode.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -45,6 +46,7 @@ namespace DicsordBot
         {
             string filterLow = filter.ToLower();
 
+            //filter for all known attributes (ignore case)
             if (file.Name.ToLower().Contains(filterLow) || file.Author.ToLower().Contains(filterLow)
                 || file.Album.ToLower().Contains(filterLow) || file.Genre.ToLower().Contains(filterLow))
             {
@@ -57,7 +59,6 @@ namespace DicsordBot
         private void filterListBox(string filter)
         {
             //clear list and apply filter
-
             if (!string.IsNullOrEmpty(filter))
             {
                 FilteredFiles.Clear();
@@ -109,50 +110,123 @@ namespace DicsordBot
             var listElement = sender as FrameworkElement;
             if (listElement != null)
             {
+                //parent is the Grid containing this button
                 var parent = listElement.Parent as FrameworkElement;
                 if (parent != null)
                 {
+                    //grandParent is the outer Grid
                     var grandParent = parent.Parent as FrameworkElement;
                     if (grandParent != null)
+                    {
+                        context_ContextMenuOpening(grandParent, null);
                         grandParent.ContextMenu.IsOpen = true;
+                    }
                 }
             }
         }
 
-        private void menu_addToQueue_Clicked(object sender, RoutedEventArgs e)
+        private void context_addToQueue_Clicked(object sender, RoutedEventArgs e)
         {
+            //play the clicked file
             uint tag = (uint)((FrameworkElement)sender).Tag;
             ListItemPlay(tag, false);
         }
 
-        private void menu_createAndAddPlaylist_Click(object sender, RoutedEventArgs e)
+        private void context_createAndAddPlaylist_Click(object sender, RoutedEventArgs e)
         {
+            //create menu, to create new playlsit
             var location = this.PointToScreen(new Point(0, 0));
             var dialog = new PlaylistAddDialog(location.X, location.Y, this.ActualWidth, this.ActualHeight);
 
-            //create new playlist from dialog result
             var result = dialog.ShowDialog();
             if (result == true)
             {
+                //create new playlist from dialog result
                 Handle.Data.Playlists.Add(new Data.Playlist(dialog.PlaylistName));
             }
+            else
+                return;
 
-            uint tag = (uint)((FrameworkElement)sender).Tag;
+            //id from last index, because it's last added from step above
+            uint listId = Handle.Data.Playlists[(int)(Handle.Data.Playlists.Count - 1)].Id;
 
-            //search for title with tag
-            foreach (var title in Handle.Data.Files)
-            {
-                if (title.Id == tag)
-                    Handle.Data.Playlists[Handle.Data.Playlists.Count - 1].Tracks.Add(title);
-            }
+            addTitleToList(listId, list_All.SelectedItems);
         }
 
         private void context_AddPlaylist_Click(object sender, RoutedEventArgs e)
         {
-            //This should crash
-            //uint tag = (uint)((FrameworkElement)sender).Tag;
+            //tag of sender is id of playlsit
+            uint listId = (uint)((FrameworkElement)sender).Tag;
 
-            //ListItemPlay(tag, false);
+            addTitleToList(listId, list_All.SelectedItems);
+        }
+
+        private void addTitleToList(uint listId, IList selectedFiles)
+        {
+            //find list from listId
+            Data.Playlist toAddList = null;
+            foreach (var list in Handle.Data.Playlists)
+            {
+                if (list.Id == listId)
+                    toAddList = list;
+            }
+
+            //all selected files
+            foreach (var file in selectedFiles)
+            {
+                //search for title with tag, add this to lsit
+                foreach (var title in Handle.Data.Files)
+                {
+                    //add
+                    if (title.Id == ((Data.FileData)file).Id && toAddList != null)
+                        toAddList.Tracks.Add(title);
+                }
+            }
+        }
+
+        private void context_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            //create context menu
+            uint tag = (uint)((FrameworkElement)sender).Tag;
+            var context = ((FrameworkElement)sender).ContextMenu;
+
+            context.Items.Clear();
+
+            //add to queue menu
+            var queueItem = new MenuItem();
+            queueItem.Header = "Add to queue";
+            queueItem.Tag = tag;
+            queueItem.Click += context_addToQueue_Clicked;
+
+            //add to context queue
+            context.Items.Add(queueItem);
+
+            //add to playlist menu
+            var playlistItem = new MenuItem();
+            playlistItem.Header = "Add to playlist...";
+            playlistItem.Tag = tag;
+
+            //new playlist item
+            var newListItem = new MenuItem();
+            newListItem.Header = "New Playlist...";
+            newListItem.Tag = tag;
+            newListItem.Click += context_createAndAddPlaylist_Click;
+
+            //add new playlist to playlistItem
+            playlistItem.Items.Add(newListItem);
+
+            //any existing playlist item
+            foreach (var list in Handle.Data.Playlists)
+            {
+                var playlist = new MenuItem();
+                playlist.Header = list.Name;
+                playlist.Tag = list.Id;
+                playlist.Click += context_AddPlaylist_Click;
+                //add each list to playlist items
+                playlistItem.Items.Add(playlist);
+            }
+            //add playlist items to context menu
+            context.Items.Add(playlistItem);
         }
     }
 
