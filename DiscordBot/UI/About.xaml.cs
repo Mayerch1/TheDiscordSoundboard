b25e29d5e494ad23684c5fe9210b1039ee557d64
Christian Mayer <it@cj-mayer.de>
2019-04-24 19:00:11 +0200

    Move Licenses to Binded List for easier maintainance

--- DiscordBot/UI/About.xaml.cs
+++ DiscordBot/UI/About.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Policy;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace DiscordBot.UI
@@ -8,13 +11,36 @@ namespace DiscordBot.UI
     /// </summary>
     public partial class About : UserControl
     {
+
+        private readonly List<License> thirdParty = new List<License>()
+        {
+            new License("Concentus", "Opus License", @"https://opus-codec.org/license/"),
+            new License("Discord.Net", "MIT License", @"https://opensource.org/licenses/MIT"),
+            new License("Gong-WPF-DragDrop", "BSD 3-Clause", @"https://github.com/punker76/gong-wpf-dragdrop/blob/dev/LICENSE"),
+            new License("MaterialDesign for XML", "MIT License", @"https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit/blob/master/LICENSE"),
+            new License("NAudio", "Microsoft Public License", @"https://github.com/naudio/NAudio/blob/master/license.txt"),
+            new License("Soundtouch(.Net)", "LGPL 2.1", @"https://raw.githubusercontent.com/owoudenberg/soundtouch.net/master/LICENSE"),
+            new License("Soundtouch.Net.NAudio", "LGPL 2.1", @"https://raw.githubusercontent.com/owoudenberg/soundtouch.net/master/LICENSE"),
+            new License("NYoutubeDL", "MIT License", @"https://licenses.nuget.org/MIT"),
+            new License("libvideo", "BSD 2-Clause", @"https://github.com/i3arnon/libvideo/blob/master/LICENSE"),
+            new License("YoutubeSearch", "GPL License", @"https://github.com/mrklintscher/YoutubeSearch#license"),
+            new License("Taglib", "LGPL 2.1", @"https://opensource.org/licenses/lgpl-2.1.php"),
+            new License("Newtonsoft.Json", "MIT License", @"https://licenses.nuget.org/MIT"),
+            new License("Microsoft .NET Library", "Microsoft", @"https://dotnet.microsoft.com/dotnet_library_license.htm"),
+        };
+
+        public List<License> ThirdParty => thirdParty;
+
+
         /// <summary>
         /// constructor of class
         /// </summary>
         public About()
         {
             InitializeComponent();
-            this.DataContext = Handle.Data.Persistent;
+            this.DataContext = this;
+
+
         }
 
         private void btn_Donate_Click(object sender, RoutedEventArgs e)
@@ -40,5 +66,23 @@ namespace DiscordBot.UI
                 }
             }
         }
+
+        
+    }
+
+    public class License
+    {
+        public License(string name, string model, string url=null)
+        {
+            Name = name;
+            Licensemodel = model;
+
+            Url = url ?? Licensemodel;
+        }
+
+
+        public string Name { get; set; }
+        public string Licensemodel { get; set; }
+        public string Url { get; set; }
     }
 }
