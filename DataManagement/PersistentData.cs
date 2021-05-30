17c48c04dfccb39e2fde835b6405014c3cbd8807
Christian <it@cj-mayer.de>
2021-05-30 19:03:38 +0200

    version bump

--- DataManagement/PersistentData.cs
+++ DataManagement/PersistentData.cs
@@ -65,7 +65,7 @@ namespace DataManagement
         /// <summary>
         /// version of this build, refers to the github release number
         /// </summary>
-        public const string version = "2.4.1";
+        public const string version = "2.4.1.1";
 
 
         /// <summary>
